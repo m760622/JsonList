cdfbb960c0e219ebdefd83836500a56e9cd23efc
Cameron Partee <cp3082@nyu.edu>
2019-12-24 20:20:40 -0500

    Inital commit with new project

---
+++ JsonList/ContentView.swift
@@ -0,0 +1,16 @@
+// Created by Cameron  Partee on 12/24/19.
+//  Copyright © 2019 Cameron Partee. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
